9f198ba4c7fa72786a796b2a22cc076535a55c4d
Stefan Ancuta <emanuelancuta@gmail.com>
2019-03-20 21:04:07 +0100

    understanding more and more, still can't make the damned thing work

--- web/main.js
+++ web/main.js
@@ -9,20 +9,20 @@ fetch(sourceURL)
   .then(function (data) {
     // Let's do something with the data we received
     var persons = data;
-    for (key in persons) { //what is key?
+    for (key in persons) { //keys are elements from json
       outputData(persons[key]);
     }
   });
-  
+
   // Change inside this function
   function outputData(person) {
     // Get the name from the link
-    var name = person.link.split(/[/]+/).pop();
+    var name = person.link.split(/[/]+/).pop(); //split at every /, keep only the last entry, which is the name
     // Make the name more readable, test with: document.write(prettyName + "<br />");
-    var prettyName = name.replace(/_/g, ' ');
+    var prettyName = name.replace(/_/g, ' ');//replace _ with space
     // Get the table HTML element
     var table = document.getElementById('persons-table');
-    // Create an empty <tr> element and add it to the 2st position of the table:
+    // Create an empty <tr> element and add it to the [key] position of the table:
     var row = table.insertRow(1);
     // Add a class to actors
     if ((new RegExp(/schauspieler/gi)).test(person.profession)) {
@@ -55,7 +55,7 @@ fetch(sourceURL)
       buttons[i].addEventListener('click', toggleRows, false);
     }
 
-    function toggleRows() {
+    function toggleRows() {//still not sure about this one
       var tableRows = document.getElementsByTagName('tr');
       for (var i = 1, length = tableRows.length; i < length; i++) {
         if (this.value == 'reset') {
